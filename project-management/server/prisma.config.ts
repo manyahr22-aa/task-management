a20458cbc032090ed581e051a9e0aae92cf4b15f
manya <manyahr22@gamil.com>
2026-04-18 13:24:58 +0530

    fix prisma build issue

---
+++ project-management/server/prisma.config.ts
@@ -0,0 +1,12 @@
+// prisma.config.ts
+import { defineConfig } from 'prisma/config'
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+})
